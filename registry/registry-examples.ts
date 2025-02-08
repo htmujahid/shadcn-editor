a48b1825f19d3037f55a97fc900976f1058999c4
Talha Mujahid <htmujahid@gmail.com>
2025-02-08 22:58:01 +0500

    fix: registry dependencies

--- registry/registry-examples.ts
+++ registry/registry-examples.ts
@@ -4,7 +4,7 @@ export const examples: Registry["items"] = [
   {
     name: "rich-text-editor-demo",
     type: "registry:example",
-    registryDependencies: ["rich-text-editor-plugin"],
+    registryDependencies: ["https://shadcn-editor.vercel.app/r/rich-text-editor-plugin.json"],
     files: [
       {
         path: "examples/rich-text-editor-demo.tsx",
@@ -15,7 +15,7 @@ export const examples: Registry["items"] = [
   {
     name: "actions-demo",
     type: "registry:example",
-    registryDependencies: ["actions-plugin", "button"],
+    registryDependencies: ["https://shadcn-editor.vercel.app/r/actions-plugin.json", "button"],
     files: [
       {
         path: "examples/actions-demo.tsx",
@@ -26,7 +26,7 @@ export const examples: Registry["items"] = [
   {
     name: "toolbar-demo",
     type: "registry:example",
-    registryDependencies: ["toolbar-plugin", "button"],
+    registryDependencies: ["https://shadcn-editor.vercel.app/r/toolbar-plugin.json", "button"],
     files: [
       {
         path: "examples/toolbar-demo.tsx",
